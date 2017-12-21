151dc49207343bcb47ff70f80932484b340a49f2
laolang2016 <xiaodaima2016@163.com>
2017-12-21 10:35:15 +0800

    bookshop-web/index.html banner search order cat

--- assets/js/lib/web/index.js
+++ assets/js/lib/web/index.js
@@ -1,6 +1,7 @@
 $(function () {
     console.log('网上书店');
 
+    // header 送至
     $('#send-to').mouseover(function () {
         $(this).find('#send-list').show();
         $(this).find('span').addClass('hover');
@@ -17,5 +18,14 @@ $(function () {
 
     $('#send-list').find('a').click(function () {
         $('#send-to').find('span').find('i').text($(this).text());
+    });
+
+    // 搜索框选择搜索类别
+    $('#search-type-select').mousemove(function () {
+        $('#search-type-list').show();
+    });
+    $('#search-type-list').find('li').click(function () {
+        $('#search-type').val($(this).find('a').attr('title'));
+        $(this).parent().hide();
     });
 });
